6c1fc7aab1faf593d23df9273e522669c221cc0e
admin <ivasisenvolodimir@gmail.com>
2026-07-17 13:04:11 +0200

    Add InventoryPage with adding to cart and cart link

---
+++ SauceDemo.UITests/Pages/InventoryPage.cs
@@ -0,0 +1,26 @@
+using OpenQA.Selenium;
+
+namespace SauceDemo.UITests.Pages;
+
+public class InventoryPage
+{
+    private IWebDriver _driver;
+    
+    public InventoryPage(IWebDriver driver)
+    {
+        _driver = driver;
+    }
+    
+    private By addToCartBtn = By.CssSelector("[data-test='add-to-cart-sauce-labs-backpack']");
+    private By cartLinkBtn = By.CssSelector("[data-test='shopping-cart-link']");
+
+    public void AddToCartClick()
+    {
+        _driver.FindElement(addToCartBtn).Click();
+    }
+
+    public void CartLinkClick()
+    {
+        _driver.FindElement(cartLinkBtn).Click();
+    }
+}
