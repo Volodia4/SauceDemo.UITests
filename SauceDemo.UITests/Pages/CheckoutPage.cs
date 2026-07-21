a8cc2ab2491fd1c4891b77c2f08d08f4a3b27d40
admin <ivasisenvolodimir@gmail.com>
2026-07-21 13:22:01 +0200

    Add CheckoutPage.cs

---
+++ SauceDemo.UITests/Pages/CheckoutPage.cs
@@ -0,0 +1,42 @@
+using OpenQA.Selenium;
+
+namespace SauceDemo.UITests.Pages;
+
+public class CheckoutPage
+{
+    private IWebDriver _driver;
+
+    public CheckoutPage(IWebDriver driver)
+    {
+        _driver = driver;
+    }
+
+    private By firstNameInput = By.CssSelector("[data-test='firstName']");
+    private By lastNameInput = By.CssSelector("[data-test='lastName']");
+    private By postalCodeInput = By.CssSelector("[data-test='postalCode']");
+    private By errorMessage = By.CssSelector("[data-test='error']");
+    private By cancelBtn = By.CssSelector("[data-test='cancel']");
+    private By continueBtn = By.CssSelector("[data-test='continue']");
+
+    public void EnterCheckoutInfo(string firstName, string lastName, string postalCode)
+    {
+        _driver.FindElement(firstNameInput).SendKeys(firstName);
+        _driver.FindElement(lastNameInput).SendKeys(lastName);
+        _driver.FindElement(postalCodeInput).SendKeys(postalCode);
+    }
+
+    public string GetErrorMessage()
+    {
+        return _driver.FindElement(errorMessage).Text;
+    }
+
+    public void CancelClick()
+    {
+        _driver.FindElement(cancelBtn).Click();
+    }
+
+    public void ContinueClick()
+    {
+        _driver.FindElement(continueBtn).Click();
+    }
+}
